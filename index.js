78a1a6215b4981825b80e23855a67c46ed05f0b1
mlgvla <mlgerardvla@gmail.com>
2021-07-16 20:32:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, callback){
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))   
+    }
+    return newArray
+}
+
+function reduce(array, callback, startingPoint) {
+    let memo;
+
+    if (startingPoint) {
+        memo = startingPoint
+        for (let i = 0; i < array.length; i++) {
+            memo = callback(array[i], memo)  
+         }
+    } else {
+        memo = array[0]
+        for (let i = 1; i < array.length; i++) {
+            memo = callback(array[i], memo)  
+         }
+    }
+    return memo
+}
